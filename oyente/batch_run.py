2706f768d24334bcd43adf28354dbd58d86d67a7
ZarIliv <2547714+ZarIliv@users.noreply.github.com>
2025-07-25 14:30:59 +0200

    Migrate batch_run.py to Python3

--- oyente/batch_run.py
+++ oyente/batch_run.py
@@ -3,7 +3,7 @@ import glob
 from tqdm import tqdm
 import os
 import sys
-import urllib2
+import urllib.request
 
 contract_dir = 'contract_data'
 
@@ -11,7 +11,7 @@ cfiles = glob.glob(contract_dir+'/contract1.json')
 
 cjson = {}
 
-print "Loading contracts..."
+print("Loading contracts...")
 
 for cfile in tqdm(cfiles):
 	cjson.update(json.loads(open(cfile).read()))
@@ -19,13 +19,13 @@ for cfile in tqdm(cfiles):
 results = {}
 missed = []
 
-print "Running analysis..."
+print("Running analysis...")
 
 contracts = cjson.keys()
 
 if os.path.isfile('results.json'):
 	old_res = json.loads(open('results.json').read())
-	old_res = old_res.keys()
+	old_res = list(old_res.keys())
 	contracts = [c for c in contracts if c not in old_res]
 
 cores=0
@@ -34,8 +34,8 @@ job=0
 if len(sys.argv)>=3:
 	cores = int(sys.argv[1])
 	job = int(sys.argv[2])
-	contracts = contracts[(len(contracts)/cores)*job:(len(contracts)/cores)*(job+1)]
-	print "Job %d: Running on %d contracts..." % (job, len(contracts))
+	contracts = contracts[(len(contracts)//cores)*job:(len(contracts)//cores)*(job+1)]
+	print("Job %d: Running on %d contracts..." % (job, len(contracts)))
 
 for c in tqdm(contracts):
 	with open('tmp.evm','w') as of:
@@ -49,6 +49,6 @@ for c in tqdm(contracts):
 		of.write(json.dumps(results,indent=1))
 	with open('missed.json', 'w') as of:
 		of.write(json.dumps(missed,indent=1))
-	# urllib2.urlopen('https://dweet.io/dweet/for/oyente-%d-%d?completed=%d&missed=%d&remaining=%d' % (job,cores,len(results),len(missed),len(contracts)-len(results)-len(missed)))
+	# urllib.request.urlopen('https://dweet.io/dweet/for/oyente-%d-%d?completed=%d&missed=%d&remaining=%d' % (job,cores,len(results),len(missed),len(contracts)-len(results)-len(missed)))
 
-print "Completed."
+print("Completed.")
